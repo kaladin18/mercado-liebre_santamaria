561db568077bd8028b36768ff641e7e13ad2d7b3
kaladin18 <francosantamaria.sm@gmail.com>
2022-02-09 22:20:53 -0300

    agregado procfile para correr app en Heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(3000, () => console.log("servidor corriendo en puerto 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("servidor corriendo en puerto 3000"));
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
 
