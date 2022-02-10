0b3e6b97e9f68b1e3f4432237cc105b3d51d0065
kaladin18 <francosantamaria.sm@gmail.com>
2022-02-09 22:05:17 -0300

    terminados los code reviews

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static(path.resolve("./public")));
+app.use(express.static(path.resolve(__dirname,"./public")));
 
 app.listen(3000, () => console.log("servidor corriendo en puerto 3000"));
 
-app.get("/", (req, res) => res.sendFile(path.resolve("./views/home.html")));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+
+app.post("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.post("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
